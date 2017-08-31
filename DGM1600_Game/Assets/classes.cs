7b448a19b903d52e62aa9633de2b00850d991a58
Unknown <10706843@my.uvu.edu>
2017-08-30 20:15:43 -0600

    classes
    created 3 sub classes for assignment.

---
+++ DGM1600_Game/Assets/classes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+		public float fuel = 0.01f;
+
+		private string name = "Bob";
+
+
+	}
+	
+	public class Two {
+		public string message = "I'M PICKLE RICK!";
+		private int iQ = 228;
+		public int maxMana = 500000 
+
+	}
+
+	public class Three {
+		public string message = "Look what you made me do.";
+		private int attack = 200;
+		public float greatness = 0.0000001;
+	}
+}
