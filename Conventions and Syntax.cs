fd9753f94a35f77b1ec1c6933116bc600f288495
Unknown <10706843@my.uvu.edu>
2017-09-11 20:18:48 -0600

    Conventions and syntax

---
+++ Conventions and Syntax.cs
@@ -0,0 +1,26 @@
+﻿// syntax is what we call the computer error checking system for code. 
+private int health = "Max";
+
+// this underline red is the syntax. This is because I have used a string for an int. Anything syntax catches will underline in red and cause the code to not work.
+
+public float fuel = ".01";
+
+//the above example is wrong because although its a float, we did not follow the float with an "f"
+
+//conventions is kind of like the grammer of coding. It makes it easier for humans to read, keeps things clean and organized.
+
+public class HelloWorld : MonoBehaviour {
+
+	string message = "Help Me!";
+
+//for example, the convention or grammer above is the indent. You don't have to have it to make the code run, but it makes it easy on the human eye to see that the information that is indented belongs to the class above. 
+
+public int mana = "3";{ }
+
+//in the above example, you have the brakets onthe same line. This will still work, but for it to follow coding conventions, it needs to look like below.
+
+public interface mana = "3";{
+	
+}
+
+
