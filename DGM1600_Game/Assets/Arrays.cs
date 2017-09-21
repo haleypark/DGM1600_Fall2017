3e0f2c14ab0102f637b3d83662cb88c37c3909b3
Unknown <10706843@my.uvu.edu>
2017-09-20 20:11:31 -0600

    started assignment Arrays

---
+++ DGM1600_Game/Assets/Arrays.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+	public string[] heros = new string[6];
+
+	
+
+	
+
+	// Use this for initialization
+	void Start () {
+	heros[0] = "Tyrande";
+	heros[1] = "Malfurian";
+	heros[2] = "Diablo";
+	heros[3] = "Nova";
+	heros[4] = "Lucio";
+	heros[5] = "Lunara";
+		
+	print(heros[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
