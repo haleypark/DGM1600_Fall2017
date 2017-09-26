a0d80c3351553680d39edda561ed75657f699fab
Unknown <10706843@my.uvu.edu>
2017-09-25 19:59:08 -0600

    switch and whileloops

---
+++ DGM1600_Game/Assets/Switches.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour {
+	public string townCenter;
+
+	public int loveMeter; 
+
+	// Use this for initialization
+	void Start () {
+		switch(townCenter){
+			case "main": 
+				print("welcome to Main Street!");
+			break;
+			case "blacksmith":
+				print("The blacksmtih grumbles as you pick through the sword bin.");
+			break;
+			case "bakery":
+				print("Mmmmnnnnnn baked goods.");
+			break;
+			case "morgue":
+				print("Peeeeeeeeeeyewww!");
+			break;
+			default:
+			print("I don't know what you're talking about!");
+			break;
+			}
+		
+		switch(loveMeter){
+			case 1:
+				print("You're crazy!");
+				break;
+			case 2:
+				print("Are you sure?");
+				break;
+			case 3:
+				print("Everything will be okay.");
+				break;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DGM1600_Game/Assets/WhileLoops.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour {
+
+	public int bottles = 100;
+
+
+	// Use this for initialization
+	void Start () {
+		while(bottles > 0){
+			bottles -- ;
+			print(bottles + " bottles of beer on the way!");
+			
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
