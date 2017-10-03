d12d1176f499e1a5967559fa05b9bbcd88c496fc
Unknown <10706843@my.uvu.edu>
2017-10-02 19:56:29 -0600

    For Loops

---
+++ DGM1600_Game/Assets/For_Loops.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class For_Loops : MonoBehaviour {
+	// public int bottles = 100;
+	void Start () {
+		// while(bottles > 0)
+		// 	print(bottles+"bottles of beer onthe wall.");
+		// 	bottles -- ;
+		for(int bottles = 100; bottles > 0; bottles -- ){
+			print(bottles+"bottles of beer onthe wall.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
