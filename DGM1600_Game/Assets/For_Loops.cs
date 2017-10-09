ebd23678a16faf5a4c77079e04e63868f42171b6
Unknown <10706843@my.uvu.edu>
2017-10-08 23:47:04 -0600

    for loops

--- DGM1600_Game/Assets/For_Loops.cs
+++ DGM1600_Game/Assets/For_Loops.cs
@@ -3,17 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class For_Loops : MonoBehaviour {
-	// public int bottles = 100;
+//a for loop is a loop that does a bit of the same thing as the other loops, but everything is organized into one line.
+//we start with for and then (). in the (), we initalize our varialbe, make a conditional and then and count up or down if we wish with -- or ++.
+//if we want to initalize our varialbe outside of the for loop, we need to start it with ;. we also need a ; after each of the 3 parts. 	
+
+public int pizza = 8;
+
+public int teamMates = 4;
+
 	void Start () {
-		// while(bottles > 0)
-		// 	print(bottles+"bottles of beer onthe wall.");
-		// 	bottles -- ;
+	
 		for(int bottles = 100; bottles > 0; bottles -- ){
 			print(bottles+"bottles of beer onthe wall.");
 		}
-	}
 	
-	// Update is called once per frame
+	//in the one below, we stated the variable outside of the loop.
+		for(int pizza; pizza>0; pizza -- ){
+			print("I have" + pizza + "pizza slices left.");
+		}
+	//in the one below, we are comparing our int to another in that exsists rather than a number we asign. 
+		for(int healthPotions = 0; teamMates > healthPotions; healthPotions ++){
+			print("I need enough potions for all my teammates!");
+		}
+		for(int mathProblems = 50; mathProblems > 0; mathProblems --){
+			print("Another math problem down!");
+		}
+		for(int videos = 0; videos < 10; videos ++){
+			print("Gotta what all the videos in my list.");
+		}
+		for(int homeworkHours = 100; homeworkHours > 0; homeworkHours --){
+			print("Gotta get in my 100 hours of homework a week!");
+		}
+		for(int poptarts = 4; poptarts > 0; poptarts --){
+			print("I want more poptarts!");
+		}
+		for(int shots = 6; shots > 0; shots --){
+			print("Take another shot!");
+		}
+		for(int mana = 0; mana < 100; mana ++){
+			print("I need more mana!");
+		}
+	}
+		
+	
+	
 	void Update () {
 		
 	}
--- DGM1600_Game/Assets/Function.cs
+++ DGM1600_Game/Assets/Function.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// void function is an operation in Csharp that proccesses information. because we use the word void, we have a function that will not give us an output or return. We can still get a print.
+//for the first one, we named the function void deathstar. We have to put the deathstar function inthe void start function. Then rather than assigning it a number, we input an already establish variable.
+//it will use the int from laserblast as the int for vent, which we declared an int inthe void deathstar function. 
+
 public class Function : MonoBehaviour {
 	public int laserBlast;
+
+	public bool positiveNumber = true;
+
+	public int rabbits = 2;
 	// Use this for initialization
 	void Start () {
 		Deathstar(laserBlast);
+
+		Addition(positiveNumber);
 	
+		Hunger(rabbits);
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		
+
 	}
 
-	void Deathstar (int vent){
+void Deathstar (int vent){
 		print("That's no moon, that's a space station.");
 
 		if(vent == 1){
@@ -28,4 +39,23 @@ public class Function : MonoBehaviour {
 			print("Death Star blows up!");
 		}
 	}
+void Addition (bool num){
+	if(num){
+		print("This was addition.");
+	}
+	else(!num){
+		print("This was not addition");
+
+	}
+	
+}
+void Hunger (int food){
+	if(food > 3){
+		print("we have enough for the night.");
+	}
+	else{
+		print("we must hunt for more food.");
+	}
+}
+void 
 }
